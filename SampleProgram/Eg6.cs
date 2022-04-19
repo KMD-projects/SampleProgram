26f55f4d8e6c584e82cb11780ea32f1a2abaf5ab
Emrys <myolwinoo919@gmail.com>
2022-04-19 19:00:59 +0630

    added eg6

---
+++ SampleProgram/Eg6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SampleProgram
+{
+    public class Eg6
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write(j+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
