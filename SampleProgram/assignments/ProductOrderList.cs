3fe2c670118018b056e15368c5fed6b7b90fa94a
Emrys <myolwinoo919@gmail.com>
2022-04-19 19:01:42 +0630

    update ProductOrderList with classes and functions

--- SampleProgram/assignments/ProductOrderList.cs
+++ SampleProgram/assignments/ProductOrderList.cs
@@ -5,62 +5,129 @@ namespace SampleProgram
     {
         static void Main()
         {
-            int price1, price2, quantity1, quantity2, amount1, amount2, totalAmount, totalQuantity, discountPercent, taxPercent;
-            double discountAmount, taxAmount, netAmount;
+            ProductOrder productOrder = new();
+            productOrder.PrepareOrder();
+            productOrder.PrintOrderInfo();
 
-            try
+            Console.Read();
+        }
+    }
+
+    class ProductOrder
+    {
+        int totalAmount, totalQuantity, discountPercent, taxPercent;
+        double discountAmount, taxAmount, netAmount;
+
+        Product product1, product2;
+
+        public void PrepareOrder()
+        {
+            product1 = new Product
             {
-                Console.Write("Enter price 1: ");
-                price1 = Convert.ToInt32(Console.ReadLine());
+                name = "Laptopi5",
+                brand = "Lenovo",
+                category = "Laptop"
+            };
 
-                Console.Write("Enter quantity 1: ");
-                quantity1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Enter price 2: ");
-                price2 = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Enter quantity 2: ");
-                quantity2 = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Enter discount(%): ");
-                discountPercent = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Enter tax(%): ");
-                taxPercent = Convert.ToInt32(Console.ReadLine());
-
-                amount1 = price1 * quantity1;
-                amount2 = price2 * quantity2;
-
-                totalAmount = amount1 + amount2;
-                totalQuantity = quantity1 + quantity2;
-
-                // calculate disount from total amount
-                discountAmount = (totalAmount * ((float)discountPercent / 100));
-                discountAmount = Math.Round(discountAmount, 2);
-                netAmount = totalAmount - discountAmount;
-
-                // calculate tax from discounted amount
-                taxAmount = (netAmount * ((float)taxPercent / 100));
-                taxAmount = Math.Round(taxAmount, 2);
-                netAmount += taxAmount;
-
-                Console.WriteLine("\t\t\tProduct Order List");
-                Console.WriteLine("Product Name\tBranch\tCategory\tPrice\tQuantity\tAmount");
-                Console.WriteLine("Laptopi5\tLenovo\tLaptop\t\t" + price1 + "\t" + quantity1 + "\t\t" + amount1);
-                Console.WriteLine("WirelessKB\tLenovo\tKB\t\t" + price2 + "\t" + quantity2 + "\t\t" + amount2);
-                Console.WriteLine("");
-                Console.WriteLine("\t\t\t\t\t\tTotal Amount\t" + totalAmount);
-                Console.WriteLine("\t\t\t\t\t\tTotal Quantity\t" + totalQuantity);
-                Console.WriteLine("");
-                Console.WriteLine("\t\t\t\t\t\tDiscount(" + discountPercent +"%)" + "\t" + discountAmount);
-                Console.WriteLine("\t\t\t\t\t\tTax(" + taxPercent + "%)" + "\t" + taxAmount);
-                Console.WriteLine("");
-                Console.WriteLine("\t\t\t\t\t\tNet Amount\t" + netAmount);
-            }
-            catch (Exception)
+            product2 = new Product
             {
-                Console.WriteLine("Invalid data.");
-            }
+                name = "WirelessKB",
+                brand = "Lenovo",
+                category = "KB"
+            };
+
+            product1.PrepareData();
+            product2.PrepareData();
+
+            discountPercent = InputHelper.GetInt("Enter discount(%): ");
+            taxPercent = InputHelper.GetInt("Enter tax(%): ");
+
+            CalculateTotal();
+            CalculateDiscount();
+            CalculateTax();
+        }
+
+        public void PrintOrderInfo()
+        {
+            Console.WriteLine("\t\t\tProduct Order List");
+            Console.WriteLine("Product Name\tBranch\tCategory\tPrice\tQuantity\tAmount");
+            Console.WriteLine(product1.name + "\t" + product1.brand + "\t" + product1.category + "\t\t" + product1.price + "\t" + product1.quantity + "\t\t" + product1.amount);
+            Console.WriteLine(product2.name + "\t" + product2.brand + "\t" + product2.category + "\t\t" + product2.price + "\t" + product2.quantity + "\t\t" + product2.amount);
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t\t\tTotal Amount\t" + totalAmount);
+            Console.WriteLine("\t\t\t\t\t\tTotal Quantity\t" + totalQuantity);
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t\t\tDiscount(" + discountPercent + "%)" + "\t" + discountAmount);
+            Console.WriteLine("\t\t\t\t\t\tTax(" + taxPercent + "%)" + "\t" + taxAmount);
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t\t\t\tNet Amount\t" + netAmount);
+        }
+
+        private void CalculateTotal()
+        {
+            totalAmount = product1.amount + product2.amount;
+            totalQuantity = product1.quantity + product2.quantity;
+        }
+
+        // calculate disount from total amount
+        private void CalculateDiscount()
+        {
+            discountAmount = totalAmount * ((float)discountPercent / 100);
+            discountAmount = Math.Round(discountAmount, 2);
+            netAmount = totalAmount - discountAmount;
+        }
+
+        // calculate tax from discounted amount
+        private void CalculateTax()
+        {
+            taxAmount = netAmount * ((float)taxPercent / 100);
+            taxAmount = Math.Round(taxAmount, 2);
+            netAmount += taxAmount;
+        }
+    }
+
+    class Product
+    {
+        public String name, brand, category;
+        public int price, quantity, amount;
+
+        private void CalculateAmount()
+        {
+            amount = price * quantity;
+        }
+
+        public void PrepareData()
+        {
+            price = InputHelper.GetInt("Enter price for " + name +": ");
+            quantity = InputHelper.GetInt("Enter quantity for " + name + ": ");
+
+            CalculateAmount();
+        }
+    }
+
+    // simple helper for getting input from user with safety checks
+    class InputHelper
+    {
+        public static int GetInt(String text)
+        {
+            int Input = 0;
+            bool IsValid;
+            do
+            {
+                try
+                {
+                    Console.Write(text);
+                    Input = Convert.ToInt32(Console.ReadLine());
+                    IsValid = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input! Please try again.");
+                    IsValid = false;
+                }
+            } while (!IsValid);
+
+            return Input;
         }
     }
 }
